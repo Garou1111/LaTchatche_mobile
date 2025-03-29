8b42a5d5db33c4df611eaf28169b07be334ce3d5
Clément Voisin <clembs@clembs.com>
2025-03-29 18:10:39 +0100

    feat: classe wrapper Api

---
+++ lib/models/api.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:latchatche_mobile/screens/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Api {
+  static Future<String> getToken() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final token = sharedPreferences.getString('session_token');
+
+    if (token == null) {
+      throw Exception('No session token found');
+    }
+
+    return token;
+  }
+
+  // Requête GET pour récupérer les données
+  static Future<http.Response> get(
+    String resource, {
+    bool authed = true,
+  }) async {
+    String? token;
+    if (authed) {
+      token = await getToken();
+    }
+
+    final response = await http.get(
+      Uri.parse('${dotenv.env['BASE_URL']}$resource'),
+      headers: {
+        if (authed && token != null) 'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+
+    // si le token est expiré ou invalide
+    // on navigue vers la page de login et on supprime le token
+    if (response.statusCode == 401) {
+      GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+      BuildContext? context = navigatorKey.currentContext;
+
+      if (context != null && context.mounted) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (Route<dynamic> route) => false,
+        );
+
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.remove('session_token');
+      }
+    }
+
+    return response;
+  }
+}
