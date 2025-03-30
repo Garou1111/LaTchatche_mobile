a2328c6f66c07890fad249d8742ec89f18f2665f
Clément Voisin <clembs@clembs.com>
2025-03-30 21:26:02 +0200

    feat(screens/discussions): voir ses propres salons

--- lib/screens/discussions.dart
+++ lib/screens/discussions.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:latchatche_mobile/models/channel.dart';
+import 'package:latchatche_mobile/screens/channel.dart';
 
 class DiscussionsScreen extends StatefulWidget {
   const DiscussionsScreen({super.key});
@@ -8,8 +10,45 @@ class DiscussionsScreen extends StatefulWidget {
 }
 
 class _DiscussionsScreenState extends State<DiscussionsScreen> {
+  late Future<List<Channel>> channels;
+
+  @override
+  void initState() {
+    super.initState();
+    channels = Channel.getMyChannels();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("TODO"));
+    return FutureBuilder<List<Channel>>(
+      future: channels,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Column(
+            children:
+                snapshot.data!.map((Channel channel) {
+                  return ListTile(
+                    onTap:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (context) => ChannelScreen(channel: channel),
+                          ),
+                        ),
+                    leading: Icon(Icons.tag),
+                    title: Text(channel.name),
+                    subtitle: Text('${channel.messageCount} messages'),
+                  );
+                }).toList(),
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        //  Montrer un indicateur de chargement pendant le chargement des données
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
