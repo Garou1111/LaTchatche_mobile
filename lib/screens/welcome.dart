e7ddf806eaab0cc5d3d4c7bcdafe456378ed0f4e
Clément Voisin <clembs@clembs.com>
2025-04-02 08:25:09 +0200

    fix(screens/welcome): on retire le scaffold de l'écran

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -6,57 +6,55 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        alignment: Alignment.bottomCenter,
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Icon(
-                Icons.chat_outlined,
-                size: 64,
-                color: Theme.of(context).colorScheme.primary,
+    return Container(
+      padding: const EdgeInsets.all(16),
+      alignment: Alignment.bottomCenter,
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              Icons.chat_outlined,
+              size: 64,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              "Bienvenue sur La Tchatche !",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              "Discutez avec vos amis et de parfaits inconnus des sujets qui vous passionne !",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 32),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginScreen(),
+                    ),
+                  );
+                },
+                child: const Text('Se connecter'),
               ),
-              const SizedBox(height: 16),
-              Text(
-                "Bienvenue sur La Tchatche !",
-                style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(height: 4),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  // todo
+                },
+                child: const Text("S'inscrire"),
               ),
-              const SizedBox(height: 4),
-              Text(
-                "Discutez avec vos amis et de parfaits inconnus des sujets qui vous passionne !",
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 32),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  },
-                  child: const Text('Se connecter'),
-                ),
-              ),
-              const SizedBox(height: 4),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: () {
-                    // todo
-                  },
-                  child: const Text("S'inscrire"),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
