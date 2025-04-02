56adee5a7b79e69427bd86eefb2a1a191ec077ea
Clément Voisin <clembs@clembs.com>
2025-04-02 08:24:07 +0200

    feat(models/api): méthode post pour envoyer des données

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -34,6 +36,49 @@ class Api {
         if (authed && token != null) 'Authorization': 'Bearer $token',
         'Content-Type': 'application/json',
       },
+    );
+
+    // si le token est expiré ou invalide
+    // on navigue vers la page de login et on supprime le token
+    if (response.statusCode == 401 && loginRedirect) {
+      GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+      BuildContext? context = navigatorKey.currentContext;
+
+      if (context != null && context.mounted) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (Route<dynamic> route) => false,
+        );
+
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.remove('session_token');
+      }
+    }
+
+    return response;
+  }
+
+  // Requête POST pour envoyer des données
+  static Future<http.Response> post(
+    String resource,
+    Map<String, dynamic> body, {
+    bool authed = true,
+    bool loginRedirect = true,
+  }) async {
+    String? token;
+    if (authed) {
+      token = await getToken();
+    }
+
+    final response = await http.post(
+      Uri.parse('${dotenv.env['BASE_URL']}$resource'),
+      headers: {
+        // Si l'utilisateur est connecté, on ajoute le token dans les headers
+        if (authed && token != null) 'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(body),
     );
 
     // si le token est expiré ou invalide
