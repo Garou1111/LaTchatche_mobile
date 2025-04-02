ac31532c88cb3ba9fdb7ac4b4178d0b4ce0d9c76
Clément Voisin <clembs@clembs.com>
2025-04-02 08:22:12 +0200

    feat(main): navigation rail pour les écrans larges

--- lib/tchatche.dart
+++ lib/tchatche.dart
@@ -12,23 +12,31 @@ class Tchatche extends StatefulWidget {
 
 class Page {
   final String title;
-  final Icon icon;
+  final IconData icon;
+  final IconData? selectedIcon;
   final Widget widget;
 
-  Page({required this.title, required this.icon, required this.widget});
+  Page({
+    required this.title,
+    required this.icon,
+    required this.selectedIcon,
+    required this.widget,
+  });
 }
 
 class _TchatcheState extends State<Tchatche> {
   int currentPageIndex = 0;
 
-  static List<Page> pages = <Page>[
+  static final List<Page> pages = <Page>[
     Page(
-      icon: Icon(Icons.tag),
+      icon: Icons.tag_outlined,
+      selectedIcon: Icons.tag,
       title: 'Discussions',
       widget: DiscussionsScreen(),
     ),
     Page(
-      icon: Icon(Icons.public),
+      icon: Icons.explore_outlined,
+      selectedIcon: Icons.explore,
       title: 'Salons publics',
       widget: PublicChannelsScreen(),
     ),
@@ -36,45 +44,113 @@ class _TchatcheState extends State<Tchatche> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(pages.elementAt(currentPageIndex).title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            tooltip: 'Créer un salon',
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.account_circle_outlined),
-            tooltip: "Mon profil",
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (_) {
-                  return const AccountModal();
-                },
-              );
+    return MediaQuery.of(context).size.width < 600
+        ? Scaffold(
+          appBar: _buildAppBar(context),
+          bottomNavigationBar: NavigationBar(
+            onDestinationSelected: (int index) {
+              setState(() {
+                currentPageIndex = index;
+              });
             },
+            selectedIndex: currentPageIndex,
+            destinations:
+                pages.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  Page page = entry.value;
+                  return NavigationDestination(
+                    icon: Icon(
+                      currentPageIndex == index ? page.selectedIcon : page.icon,
+                    ),
+                    label: page.title,
+                  );
+                }).toList(),
           ),
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations:
-            pages.map((Page page) {
-              return NavigationDestination(icon: page.icon, label: page.title);
-            }).toList(),
-      ),
-      body: SingleChildScrollView(
-        child: pages.elementAt(currentPageIndex).widget,
-      ),
+          body: SingleChildScrollView(
+            child: pages.elementAt(currentPageIndex).widget,
+          ),
+        )
+        : Scaffold(
+          body: Row(
+            children: <Widget>[
+              NavigationRail(
+                leading: Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child:
+                      MediaQuery.of(context).size.width >= 800
+                          ? Text(
+                            'La Tchatche',
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          )
+                          : Icon(Icons.message_outlined),
+                ),
+                labelType:
+                    MediaQuery.of(context).size.width >= 800
+                        ? NavigationRailLabelType.none
+                        : NavigationRailLabelType.selected,
+                extended: MediaQuery.of(context).size.width >= 800,
+                groupAlignment: -1,
+                destinations:
+                    pages.asMap().entries.map((entry) {
+                      int index = entry.key;
+                      Page page = entry.value;
+
+                      return NavigationRailDestination(
+                        icon: Icon(
+                          currentPageIndex == index
+                              ? page.selectedIcon
+                              : page.icon,
+                        ),
+                        label: Text(page.title),
+                      );
+                    }).toList(),
+                selectedIndex: currentPageIndex,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    currentPageIndex = index;
+                  });
+                },
+              ),
+              const VerticalDivider(thickness: 1, width: 1),
+              Expanded(
+                child: Column(
+                  children: [
+                    _buildAppBar(context),
+                    SingleChildScrollView(
+                      child: pages.elementAt(currentPageIndex).widget,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Text(pages.elementAt(currentPageIndex).title),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          tooltip: 'Créer un salon',
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.account_circle_outlined),
+          tooltip: "Mon profil",
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (_) {
+                return const AccountModal();
+              },
+            );
+          },
+        ),
+      ],
     );
   }
 }
